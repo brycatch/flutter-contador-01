367c0b54c9eafedf5bac864fed59f92346e23ca9
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-01-28 20:23:24 -0600

    Sección terminada

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -12,31 +12,68 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Título'), centerTitle: true, elevation: 10),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Número de tabs',
-              style: _labelStyle,
-            ),
-            Text(
-              '$_count',
-              style: _labelStyle,
-            )
-          ],
+        appBar: AppBar(title: Text('Título'), centerTitle: true, elevation: 10),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Número de tabs',
+                style: _labelStyle,
+              ),
+              Text(
+                '$_count',
+                style: _labelStyle,
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _count++;
-          });
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: _reset,
+        ),
+        Expanded(
+            child: SizedBox(
+          width: 5.0,
+        )),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _sustraer,
+        ),
+        SizedBox(
+          width: 5.0,
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _agregar,
+        ),
+      ],
     );
+  }
+
+  void _agregar() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void _sustraer() {
+    setState(() {
+      _count--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _count = 0;
+    });
   }
 }
